e1e341251e83ef8f3452904ffb45f11ef177df21
SrgMarkov <107784915+SrgMarkov@users.noreply.github.com>
2023-05-11 14:24:56 +0400

    Update bitlink_in_console.py

--- bitlink_in_console.py
+++ bitlink_in_console.py
@@ -7,7 +7,8 @@ import os
 def get_short_link(access_token, url):
     url_bitly = 'https://api-ssl.bitly.com/v4/bitlinks'
     query = {"long_url": url}
-    response = requests.post(url_bitly, headers=access_token, json=query)
+    api_auth = {'Authorization': f'Bearer {access_token}'}
+    response = requests.post(url_bitly, headers=api_auth, json=query)
     response.raise_for_status()
     bitlink = 'Битлинк: ' + response.json()['link'][8:]
     return bitlink
@@ -17,7 +18,8 @@ def get_count_clicks(access_token, url):
     parsed = urlparse(url)
     url_bitly = f'https://api-ssl.bitly.com/v4/bitlinks/{parsed.netloc}{parsed.path}/clicks/summary'
     params = {'units': '-1'}
-    response = requests.get(url_bitly, headers=access_token, params=params)
+    api_auth = {'Authorization': f'Bearer {access_token}'}
+    response = requests.get(url_bitly, headers=api_auth, params=params)
     response.raise_for_status()
     bitlink = 'По вашей ссылке прошли ' + str(response.json()['total_clicks']) + ' раз(а)'
     return bitlink
@@ -26,16 +28,18 @@ def get_count_clicks(access_token, url):
 def is_bitlink(access_token, url):
     parsed = urlparse(url)
     url_bitly = f'https://api-ssl.bitly.com/v4/bitlinks/{parsed.netloc}{parsed.path}'
-    response = requests.get(url_bitly, headers=access_token)
+    api_auth = {'Authorization': f'Bearer {access_token}'}
+    response = requests.get(url_bitly, headers=api_auth)
     return response.ok
 
 
 def main():
     load_dotenv()
-    token = {'Authorization': f'Bearer {os.getenv("BITLY_TOKEN")}'}
+    bitly_token = os.getenv("BITLY_TOKEN")
     link = input('Введите ссылку: ')
     try:
-        print(get_count_clicks(token, link)) if is_bitlink(token, link) else print(get_short_link(token, link))
+        print(get_count_clicks(bitly_token, link)) if is_bitlink(bitly_token, link)\
+            else print(get_short_link(bitly_token, link))
     except requests.exceptions.RequestException as e:
         print(f"Ошибка при выполнении запроса: {e}")
 
